f667e85995dd2b6c890c4619641a0bd01903ff79
giraffeunicycle <aashadowaa@gmail.com>
2017-10-04 15:43:48 -0400

    first commit

---
+++ DataCollector/app/src/main/java/com/datacollector/MainActivity.java
@@ -0,0 +1,244 @@
+package com.datacollector;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Bundle;
+import android.os.Environment;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity implements SensorEventListener, View.OnClickListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Button mButtonStart, mButtonStop, mButtonSave;
+    private LineChart mChart;
+    private ToneGenerator mToneGen;
+
+    private boolean mCollectingData = false;
+    private ArrayList<AccelData> mSensorData;
+    private long mStartTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // sensor
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // buttons
+        mButtonStart = (Button) findViewById(R.id.button_start);
+        mButtonStop = (Button) findViewById(R.id.button_stop);
+        mButtonSave = (Button) findViewById(R.id.button_save);
+        mButtonStart.setOnClickListener(this);
+        mButtonStop.setOnClickListener(this);
+        mButtonSave.setOnClickListener(this);
+        mButtonStop.setEnabled(false);
+
+        // chart
+        mChart = (LineChart) findViewById(R.id.chart);
+
+        // tone generator
+        mToneGen = new ToneGenerator(AudioManager.STREAM_DTMF, ToneGenerator.MAX_VOLUME);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // save data to ArrayList
+        if (mCollectingData) {
+            double x = event.values[0];
+            double y = event.values[1];
+            double z = event.values[2];
+            long timestamp = System.currentTimeMillis() - mStartTime;
+            // stop after 2 seconds
+            if (timestamp >= 2000) {
+                onClick(mButtonStop);
+            } else {
+                AccelData data = new AccelData(timestamp, x, y, z);
+                mSensorData.add(data);
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.button_start:
+                // enable/disable buttons
+                mButtonStop.setEnabled(true);
+                mButtonStart.setEnabled(false);
+
+                // set up array
+                mSensorData = new ArrayList<>();
+                mStartTime = System.currentTimeMillis();
+
+                // start collecting data
+                mCollectingData = true;
+                break;
+
+            case R.id.button_stop:
+                // stop collecting data
+                mCollectingData = false;
+
+                // enable/disable buttons
+                mButtonStop.setEnabled(false);
+                mButtonStart.setEnabled(true);
+
+                // make loud beep
+                mToneGen.startTone(ToneGenerator.TONE_DTMF_0, 100);
+
+                // plot data
+                plotData();
+                break;
+
+            case R.id.button_save:
+                // pop up Dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                LayoutInflater inflater = this.getLayoutInflater();
+                final View view = inflater.inflate(R.layout.save_dialog, null);
+                builder.setMessage(R.string.save_message)
+                        .setView(view)
+                        .setPositiveButton(R.string.button_save, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                EditText et = (EditText) view.findViewById(R.id.save_text);
+                                saveData(et.getText().toString());
+                            }
+                        });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void plotData() {
+        List<Entry> xPoints = new ArrayList<>();
+        List<Entry> yPoints = new ArrayList<>();
+        List<Entry> zPoints = new ArrayList<>();
+
+        for (AccelData data : mSensorData) {
+            xPoints.add(new Entry(data.getTimestamp(), (float) data.getX()));
+            yPoints.add(new Entry(data.getTimestamp(), (float) data.getY()));
+            zPoints.add(new Entry(data.getTimestamp(), (float) data.getZ()));
+        }
+
+        LineDataSet xDataSet = new LineDataSet(xPoints, "X");
+        xDataSet.setColor(Color.RED);
+        xDataSet.setDrawCircles(false);
+        LineDataSet yDataSet = new LineDataSet(yPoints, "Y");
+        yDataSet.setColor(Color.GREEN);
+        yDataSet.setDrawCircles(false);
+        LineDataSet zDataSet = new LineDataSet(zPoints, "Z");
+        zDataSet.setColor(Color.BLUE);
+        zDataSet.setDrawCircles(false);
+
+        LineData lineData = new LineData(xDataSet, yDataSet, zDataSet);
+        mChart.setData(lineData);
+
+        Description desc = new Description();
+        desc.setText("");
+        mChart.setDescription(desc);
+
+        mChart.invalidate();
+    }
+
+    private void saveData(String label) {
+        String filename = label.toLowerCase() + "_" + System.currentTimeMillis() + ".csv";
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/DataCollector/";
+        File dir = new File(path);
+        dir.mkdirs();
+        File file = new File(dir, filename);
+
+        try {
+            file.createNewFile();
+            FileOutputStream outputStream = new FileOutputStream(file);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+            for (AccelData data : mSensorData) {
+                outputStreamWriter.append(data.toString()).append("\n");
+            }
+            outputStreamWriter.close();
+            outputStream.flush();
+            outputStream.close();
+            Toast.makeText(this, "File \"" + filename + "\" saved.", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+            Toast.makeText(this, "Error! File not saved.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
